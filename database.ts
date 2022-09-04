239f1055543ca69111990acee6e7105c563cca0c
Nathan Gazey <nategg@pm.me>
2022-09-04 16:00:34 -0600

    chore: finalize getTodo
    - Move database functions to database interface layer.
    - Updated query param check to properly check for bad requests.
    - Updated readme to detail required env variable.

--- database.ts
+++ database.ts
@@ -1,5 +1,7 @@
 import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
-export { PoolClient } from "https://deno.land/x/postgres@v0.14.0/client.ts";
+import { PoolClient } from "https://deno.land/x/postgres@v0.14.0/client.ts";
+import { TodoList, TodoListItem, TodoListInfo } from "./models/todo.d.ts"
+
 
 // @ts-ignore We have to add the toJSON for this type for serialization later on.
 BigInt.prototype.toJSON = function() { return this.toString() }
@@ -14,3 +16,30 @@ const databaseUrl = Deno.env.get("DATABASE_URL");
  * Create a pool of database connections to use.
  */ 
 export const pgPool = new postgres.Pool(databaseUrl, 5, true);
+
+/**
+ * 
+ * @param connection connection to database to use
+ * @param name The name of the list that we want to retrieve
+ * @returns The requested todo list or undefined.
+ */
+ export async function getTodoListInfoByName(connection: PoolClient, name: string): Promise<TodoList | undefined> {
+    const todoListQueryResults = await connection.queryObject<TodoListInfo> `
+        select * from "TodoLists" where name = ${name}
+      `;
+    // First (and only possible) result or undefined.
+    return todoListQueryResults.rows?.[0] as TodoList | undefined;
+  }
+  
+  /**
+   * 
+   * @param connection connection to database to use
+   * @param ListID The list we want to retrieve items for.
+   * @returns The requested todo list items or an empty array.
+   */
+  export async function getTodoListItemsByListID(connection: PoolClient, ListID: number): Promise<Array<TodoListItem>> {
+    const items = await connection.queryObject<TodoListItem>`
+      select ID, text from "TodoListItems" where list = ${ListID}
+    `;
+    return items.rows;
+  }
--- routes/api/todo.ts
+++ routes/api/todo.ts
@@ -1,33 +1,7 @@
 import { Handlers, Status } from "$fresh/server.ts";
-import { pgPool, PoolClient } from "../../database.ts";
-import { TodoList, TodoListItem, TodoListInfo } from "../../models/todo.d.ts"
+import { pgPool, getTodoListInfoByName, getTodoListItemsByListID } from "../../database.ts";
 
-/**
- * 
- * @param connection connection to database to use
- * @param name The name of the list that we want to retrieve
- * @returns The requested todo list or undefined.
- */
-async function getTodoListInfoByName(connection: PoolClient, name: string): Promise<TodoList | undefined> {
-  const todoListQueryResults = await connection.queryObject<TodoListInfo> `
-      select * from "TodoLists" where name = ${name}
-    `;
-  // First (and only possible) result or undefined.
-  return todoListQueryResults.rows?.[0] as TodoList | undefined;
-}
 
-/**
- * 
- * @param connection connection to database to use
- * @param ListID The list we want to retrieve items for.
- * @returns The requested todo list items or an empty array.
- */
-async function getTodoListItemsByListID(connection: PoolClient, ListID: number): Promise<Array<TodoListItem>> {
-  const items = await connection.queryObject<TodoListItem>`
-    select ID, text from "TodoListItems" where list = ${ListID}
-  `;
-  return items.rows;
-}
 
   /**
    * GET request handler for the `/api/todo` endpoint. This will return a todo list based on the name provided in the request.
@@ -38,15 +12,16 @@ async function getTodo(req: Request): Promise<Response> {
   let responseBody: string | null = null;
   const queryParams = (new URL(req.url)).searchParams;
   // prevent escape characters by getting the raw string.
-  const name = String.raw`${queryParams.get("name")}`; 
-  
-  if(name === null) {
+  const name = queryParams.has("name") ? String.raw`${queryParams.get("name")}` : undefined; 
+
+  if(name === undefined) {
     // User failed to provide a name to find a todo list for.
     return new Response(null, {
       status: Status.BadRequest,
       statusText: "Query parameter 'name' is required."
     });
   }
+  console.log(typeof name);
 
   // Connect to the database.
   const connection = await pgPool.connect();
