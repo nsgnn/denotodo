159aba0e9c27d76cf65fda4205f4a702e36357bf
Nathan Gazey <nategg@pm.me>
2022-09-04 11:44:18 -0600

    Initialize Repository 🥳🏃‍♂️

---
+++ README.md
@@ -0,0 +1,21 @@
+# Headless To-do API
+
+### Usage
+
+Deno is required to run the server. This can be found [here](https://deno.land/manual@v1.25.1/getting_started/installation).
+
+Start the project in dev mode:
+
+```
+deno task start
+```
+
+Build a docker image for production:
+
+```
+docker build --build-arg GIT_REVISION=$(git rev-parse HEAD) -t denotodo .
+```
+Run a production image:
+```
+docker run -t -i -p 80:8000 deno-to-do
+```
---
+++ routes/api/todo.ts
@@ -0,0 +1,45 @@
+import { Handlers, Status } from "$fresh/server.ts";
+
+/**
+ * The exported handlers will control any request sent to the `/api/todo` endpoint. Any method that is not implemented will return a 405 response.
+ */
+export const handler: Handlers = {
+
+  /**
+   * GET request handler for the `/api/todo` endpoint. This will return a todo list based on the name provided in the request.
+   * @param req The request sent to the server by the client. This is required to contain the query param: `name`.
+   * @returns Response will be the todo list requested found based on the `Name` field. Failure to provide required fields will result in a 400 response.
+   */
+  GET(req) {
+    console.log(req.url);
+    return new Response("", {
+      status: Status.NotImplemented
+    });
+  },
+
+  /**
+   * POST request handler for the `/api/todo` endpoint. This will create a new todo list and save it to the database.
+   * @param req The request sent to the server by the client. This is required to contain the values: `Name` and `Description`.
+   * @returns Response will indicate the success of the todo list item saving. Failure to provide required fields will result in a 400 response.
+   */
+  POST(req) {
+    console.log(req.body);
+    return new Response("", {
+      status: Status.NotImplemented
+    });
+  },
+
+  /**
+   * DELETE request handler for the `/api/todo` endpoint. This will delete an existing todo list from the database.
+   * @param req The request sent to the server by the client. This is required to contain the value: `Name`.
+   * @returns Response will indicate the success of the todo list item deleting. Failure to provide required fields will result in a 400 response.
+   */
+  DELETE(req) {
+    console.log(req.body);
+    return new Response("", {
+      status: Status.NotImplemented
+    });
+  },
+  
+
+};
